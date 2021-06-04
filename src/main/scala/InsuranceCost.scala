43bf2e84245ff378f96340da414b6c5088165d5a
carlmaps <cmmapada@gmail.com>
2021-06-04 11:01:48 -0500

    added new code to get running averages

--- src/main/scala/InsuranceCost.scala
+++ src/main/scala/InsuranceCost.scala
@@ -1,3 +1,4 @@
+import org.apache.spark.sql.functions.avg
 import org.apache.spark.sql.{SQLContext, SparkSession}
 
 object InsuranceCost extends App{
@@ -12,10 +13,24 @@ object InsuranceCost extends App{
     val sqlContext:SQLContext = spark.sqlContext
 
     //read csv with options
-    val df = sqlContext.read.options(Map("inferSchema"->"true","delimiter"->",","header"->"true"))
-      .csv("insurance.csv")
-    df.show()
-    df.printSchema()
+    val insuranceDF = spark.read.format("csv")
+                  .option("inferSchema","true")
+                  .option("header", "true")
+                  .option("sep", ",").csv("insurance.csv").cache()
+
+
+//    val df = sqlContext.read.options(Map("inferSchema"->"true","delimiter"->",","header"->"true"))
+//      .csv("insurance.csv")
+    insuranceDF.show()
+    insuranceDF.printSchema()
+
+    val counter = 0
+    for(counter <- 1 to 10){
+      val sampledData = insuranceDF.sample(false, 0.5, 1)
+      val aveAge = sampledData.groupBy("sex").agg(avg("age")).show()
+      val aveBMI = sampledData.groupBy("sex").agg(avg("bmi")).show()
+      val aveCharges = sampledData.groupBy("sex").agg(avg("charges")).show()
+    }
 
   }
 
